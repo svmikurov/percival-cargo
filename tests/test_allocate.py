31a0e72fba765e50ab67b69764f1cc4505a6851b
Sergei Mikurov <454004@mail.ru>
2026-05-30 12:22:13 +0500

    Add domain model test

---
+++ tests/test_allocate.py
@@ -0,0 +1,94 @@
+"""Test the product order line allocation to batch."""
+
+import pytest
+
+from percival_cargo.domain.model import Batch, OrderLine
+
+
+def create_objects(
+    squ: str,
+    batch_qty: int,
+    line_qty: int,
+) -> tuple[Batch, OrderLine]:
+    """Create the product order line and batch."""
+    return (
+        Batch('batch-001', squ, batch_qty),
+        OrderLine('order-001', squ, line_qty),
+    )
+
+
+@pytest.mark.parametrize(
+    'squ, batch_qty, line_qty, expected_qty',
+    (
+        ('TABLE-SMALL', 20, 2, 18),
+        ('TABLE-SMALL', 2, 20, 2),
+        ('TABLE-SMALL', 20, 20, 0),
+    ),
+)
+def test_allocation(
+    squ: str,
+    batch_qty: int,
+    line_qty: int,
+    expected_qty: bool,
+) -> None:
+    """Test the product order line allocation to batch."""
+    # Arrange
+    batch, line = create_objects(squ, batch_qty, line_qty)
+
+    # Act
+    batch.allocate(line)
+
+    # Assert
+    assert batch.available_quantity == expected_qty
+
+
+def test_the_duplicate_allocation() -> None:
+    """Test that the duplicate allocation not available."""
+    # Arrange
+    batch_qty = 20
+    line_qty = 2
+    expected_qty = batch_qty - line_qty
+
+    batch, line = create_objects('TABLE-SMALL', batch_qty, line_qty)
+
+    # Act
+    batch.allocate(line)
+    batch.allocate(line)
+
+    # Assert
+    assert batch.available_quantity == expected_qty
+
+
+def test_the_different_allocation() -> None:
+    """Test that the different allocations is available."""
+    # Arrange
+    batch_qty = 20
+    line_qty = 2
+    other_line_qty = 2
+    expected_qty = batch_qty - line_qty - other_line_qty
+
+    batch, line = create_objects('TABLE-SMALL', batch_qty, line_qty)
+    other_line = OrderLine('order-002', 'TABLE-SMALL', other_line_qty)
+
+    # Act
+    batch.allocate(line)
+    batch.allocate(other_line)
+
+    # Assert
+    assert batch.available_quantity == expected_qty
+
+
+def test_the_different_line_and_batch() -> None:
+    """Test that cannot allocate if SKUs do not match."""
+    # Arrange
+    batch_qty = 20
+    line_qty = 2
+
+    batch = Batch('batch-001', 'TABLE-BIG', batch_qty)
+    line = OrderLine('order-001', 'TABLE-SMALL', line_qty)
+
+    # Act
+    batch.allocate(line)
+
+    # Assert
+    assert batch.available_quantity == batch_qty
