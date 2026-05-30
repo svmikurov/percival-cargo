34b7bb6be596045491fccdf2aaf74f7f38ee75e9
Sergei Mikurov <454004@mail.ru>
2026-05-30 13:41:41 +0500

    Add domain model interfaces

---
+++ src/percival_cargo/domain/interfaces.py
@@ -0,0 +1,64 @@
+"""Domain model interface."""
+
+from __future__ import annotations
+
+from typing import Protocol
+
+###################################################
+# Components
+###################################################
+
+
+class HasOrderID(Protocol):
+    @property
+    def order_id(self) -> str: ...
+
+
+class HasSKU(Protocol):
+    @property
+    def sku(self) -> str: ...
+
+
+class HasQuantity(Protocol):
+    @property
+    def qty(self) -> int: ...
+
+
+class HasAvailableQuantity(Protocol):
+    @property
+    def available_quantity(self) -> int: ...
+
+
+class Allocatable(Protocol):
+    def allocate(self, line: OrderLineProtocol) -> None: ...
+
+
+class DeAllocatable(Protocol):
+    def deallocate(self, line: OrderLineProtocol) -> None: ...
+
+
+class CanAllocate(Protocol):
+    def can_allocate(self, line: OrderLineProtocol) -> bool: ...
+
+
+###################################################
+# Compositions
+###################################################
+
+
+class OrderLineProtocol(
+    HasOrderID,
+    HasSKU,
+    HasQuantity,
+    Protocol,
+):
+    """Protocol for order line interface."""
+
+
+class BatchProtocol(
+    CanAllocate,
+    DeAllocatable,
+    HasAvailableQuantity,
+    Protocol,
+):
+    """Protocol for product batch interface."""
