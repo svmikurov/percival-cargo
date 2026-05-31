3fdd6210b47629c53279ae6f630a3385e22c6fd4
Sergei Mikurov <454004@mail.ru>
2026-06-01 02:40:59 +0500

    Add events

--- src/percival_cargo/domain/events.py
+++ src/percival_cargo/domain/events.py
@@ -1,6 +1,7 @@
 """Domain events."""
 
 from dataclasses import dataclass
+from datetime import date
 
 
 class Event:
@@ -12,3 +13,22 @@ class OutOfStock(Event):
     """out of stock event."""
 
     sku: str
+
+
+@dataclass
+class BatchCreated(Event):
+    """Batch created event."""
+
+    ref: str
+    sku: str
+    qty: int
+    eta: date | None = None
+
+
+@dataclass
+class AllocationRequired(Event):
+    """Allocation required event."""
+
+    order_id: str
+    sku: str
+    qty: str
