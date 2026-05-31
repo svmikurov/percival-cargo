e343024d52e9a86a625c71a2312e7384399b7b53
Sergei Mikurov <454004@mail.ru>
2026-06-01 02:46:59 +0500

    Add unit of work interface

---
+++ src/percival_cargo/domain/ports/uow.py
@@ -0,0 +1,21 @@
+"""Protocols for Unit Of Work interface."""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Protocol, Self
+
+if TYPE_CHECKING:
+    from . import components, repository
+
+
+class UnitOfWorkProtocol(
+    components.HasProducts[repository.RepositoryProtocol],
+    components.CanCommit,
+    components.CanCollectNewEvents,
+    components.CanRollback,
+    Protocol,
+):
+    """Protocol fo Unit Of Work interface."""
+
+    def __enter__(self) -> Self: ...
+    def __exit__(self, *args: object) -> None: ...
